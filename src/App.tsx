a5228c3755b5edfcb38e1984d6e7db315509d136
Terris <27739101+Terristwj@users.noreply.github.com>
2023-08-25 18:47:33 +0800

    Added routing and test pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
+import NoPage from "./pages/NoPage";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route index path="landingpage" element={<LandingPage />} />
+                    {/* <Route path="contact" element={<Contact />} /> */}
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
