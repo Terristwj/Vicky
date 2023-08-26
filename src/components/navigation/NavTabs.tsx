570684f27507a869fa7916e19b00f220b0235b20
nafutofu <naufal.syaqil@gmail.com>
2023-08-26 19:21:33 +0800

    Cleaned up the NavBar

--- src/components/navigation/NavTabs.tsx
+++ src/components/navigation/NavTabs.tsx
@@ -1,19 +1,29 @@
-import vicky from "../../img/Viiicky.png";
+
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import Container from "@mui/material/Container";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { useNavigate } from 'react-router-dom';
+import logo from '../images/icon-white.png';
 import { CardMedia } from "@mui/material";
 
-const settings = ["item 1", "item 2", "item 3"];
+import vicky from "../../img/Viiicky.png";
+
 
 function ResponsiveAppBar() {
+    let navigate = useNavigate();
+
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
         null
     );
@@ -36,78 +46,58 @@ function ResponsiveAppBar() {
         setAnchorElUser(null);
     };
 
-    return (
-        <AppBar position="static" style={{ background: "#DEE1E6" }}>
-            <Container maxWidth="xl">
-                <Toolbar>
-                    <CardMedia
-                        component="img"
-                        image={vicky}
-                        title="Vicky logo"
-                        sx={{ margin: "16px 0 6px", height: "50px", width: "auto"}}
-                    />
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "flex", md: "none" },
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        ></IconButton>
-                    </Box>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                        }}
-                    ></Box>
+    const handleColor = () => {
+        navigate('/coloridentifier')
+    };
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton
-                                onClick={handleOpenUserMenu}
-                                sx={{ mr: 20 }}
-                            >
-                                Settings
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: "45px" }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
+    const handleHome = () => {
+        navigate('/')
+    }
+
+    return (
+        <>
+            <AppBar position="sticky" style={{ background: "#DEE1E6" }}>
+                <Container maxWidth="xl">
+                    <Toolbar>
+                        <CardMedia
+                            component="img"
+                            image={vicky}
+                            title="Vicky logo"
+                            sx={{
+                                margin: "16px 0 6px",
+                                height: "50px",
+                                width: "auto",
                             }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
+                        />
+                        <Box justifyContent="center" alignItems="center" sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
+                            <Button key='Home' onClick={handleHome} sx={{ my: 0, color: 'black', display: 'block' }} >Home</Button>
+                            <Button key='Color' onClick={handleColor} sx={{ my: 0, color: 'black', display: 'block' }} >Colour Identifier</Button>
+                            <Button key='Profile' sx={{ my: 0, color: 'black', display: 'block' }} >Experience</Button>
+                            <Button key='About'  sx={{ my: 0, color: 'black', display: 'block' }} >Settings</Button>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: "flex", md: "none" },
                             }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
                         >
-                            {settings.map((setting) => (
-                                <MenuItem
-                                    key={setting}
-                                    onClick={handleCloseUserMenu}
-                                >
-                                    <Typography textAlign="center">
-                                        {setting}
-                                    </Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            ></IconButton>
+                        </Box>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: "none", md: "flex" },
+                            }}/>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
     );
 }
 export default ResponsiveAppBar;
