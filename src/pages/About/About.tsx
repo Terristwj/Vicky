886d26312d6efdf92fb951e5fb326bbf47b50383
Terris <27739101+Terristwj@users.noreply.github.com>
2023-08-31 02:43:09 +0800

    Update About.tsx

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,80 +1,62 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
-function Copyright(props: any) {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-        <Link color="inherit" href="/">
-          Viiicky
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
+import Footer from "../../components/Footer";
 
 export default function About() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-
-  return (
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://i.imgur.com/C7YaTtT.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              color: 'dark grey',
-            }}
-          >
-            <Typography  variant="h3">
-              About Us
-            </Typography>
-              <Grid container margin={3}>
-                <Typography alignContent='center' justifyContent= 'center' variant= "h5">
-                    We have a colourblind person in our group. At first, we joked around about his colourblindness but the more we listened, the more we realised that the issues he faced were not light at all. What more, the colour rectifing glasses from enchroma cost over 200 dollars which we thought was unreasonable since the ability to see colour should naturally be free for everybody. Thus we decided to build an accessible and free way for all colourblind people to be able to identify colours. 
-                </Typography>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
+    return (
+        <Grid container sx={{ height: { md: "92vh" } }}>
+            <Grid
+                item
+                xs={12}
+                md={6}
+                lg={7}
+                sx={{
+                    backgroundImage: "url(https://i.imgur.com/C7YaTtT.jpg)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    marginX: "auto",
+                    height: {
+                        xs: "100px",
+                        md: "auto",
+                    },
+                }}
+            />
+            <Grid xs={12} md={6} lg={5}>
+                <Box
+                    sx={{
+                        mt: { xs: 4, md: 8 },
+                        mx: 4,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        color: "dark grey",
+                    }}
+                >
+                    <Typography variant="h3">About Us</Typography>
+                    <Grid container margin={3}>
+                        <Typography
+                            alignContent="center"
+                            justifyContent="center"
+                            variant="h5"
+                        >
+                            We have a colourblind person in our group. At first,
+                            we joked around about his colourblindness but the
+                            more we listened, the more we realised that the
+                            issues he faced were not light at all. What more,
+                            the colour rectifing glasses from enchroma cost over
+                            200 dollars which we thought was unreasonable since
+                            the ability to see colour should naturally be free
+                            for everybody. Thus we decided to build an
+                            accessible and free way for all colourblind people
+                            to be able to identify colours.
+                        </Typography>
+                    </Grid>
+                </Box>
+                <Footer />
+            </Grid>
         </Grid>
-      </Grid>
-  );
+    );
 }
